4b94fdcd6085b216ce5b7ecf03786a8fd01dd389
AlexeyBakin <blackacsel123@gmail.com>
2022-01-30 22:34:59 +0300

    1

---
+++ src/main/java/web/service/UserService.java
@@ -0,0 +1,16 @@
+package web.service;
+
+import web.model.User;
+import java.util.List;
+
+public interface UserService {
+    List<User> getAllUsers();
+
+    User getUserById(long id);
+
+    void add(User user);
+
+    void update(User user);
+
+    void delete(long id);
+}
